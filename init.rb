cdb185e54376f4efdf16ffc550d2791095f6ae64
Marc-Andre Gagnon <magagnon@praizedmedia.com>
2010-10-12 15:36:59 -0400

    Removed deprecated rails folder

--- init.rb
+++ init.rb
@@ -1,1 +1,9 @@
-File.dirname(__FILE__) + "/rails/init.rb"
+require "authlogic_oauth"
+require "oauth_callback_filter"
+
+# Throw callback rack app into the middleware stack
+if defined? Rails.configuration && defined?(Rails.configuration.middleware)
+  Rails.configuration.middleware.use(OauthCallbackFilter)
+elsif defined? ActionController::Dispatcher && defined?(ActionController::Dispatcher.middleware)
+  ActionController::Dispatcher.middleware.use(OauthCallbackFilter)
+end
--- rails/init.rb
+++
@@ -1,9 +0,0 @@
-require "authlogic_oauth"
-require "oauth_callback_filter"
-
-# Throw callback rack app into the middleware stack
-if defined? Rails.configuration && defined?(Rails.configuration.middleware)
-  Rails.configuration.middleware.use(OauthCallbackFilter)
-elsif defined? ActionController::Dispatcher && defined?(ActionController::Dispatcher.middleware)
-  ActionController::Dispatcher.middleware.use(OauthCallbackFilter)
-end
